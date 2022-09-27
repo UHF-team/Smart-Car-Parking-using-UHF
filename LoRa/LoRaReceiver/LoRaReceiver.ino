5de3c9b0cfb0f969b06072ae9e0ae8f3a158b675
flyingman2401 <flyingman24012001@gmail.com>
2022-09-27 09:28:14 +0700

    Upload LoRa Receiver code.

---
+++ LoRa/LoRaReceiver/LoRaReceiver.ino
@@ -0,0 +1,84 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include "SerialTransfer.h"
+
+// -------------------- Configue Parameters -------------------- //
+
+const long LoRa_freq = 915E6;
+const int LoRa_SF = 7;
+const long LoRa_bw = 125E3;
+int counter, lastCounter;
+SerialTransfer myTransfer;
+#define PAYLOAD_LENGTH 19
+
+// -------------------- Send back LoRa ACK packet -------------------- //
+
+void sendACK(byte *message) {
+  int check = 0;
+  for (int i = 0; i < PAYLOAD_LENGTH; i++) {
+    check += message[i];
+  }
+  
+  LoRa.beginPacket();
+  LoRa.print(String(check));
+  LoRa.endPacket();
+  
+//  Serial.print(message);
+  Serial.print(" ACK Sent: ");
+  Serial.println(check);
+}
+
+// -------------------- Send Message to PI via UART -------------------- //
+
+void sendUARTMessage(byte *message) {
+//  message = "UHF0001123456789123";
+
+  for (int i = 0; i <= PAYLOAD_LENGTH; i++)
+    myTransfer.packet.txBuff[i]= message[i];
+  
+  myTransfer.sendData(PAYLOAD_LENGTH);
+}
+
+// -------------------- Send LoRa Message to Receiver -------------------- //
+
+void receiveMessage() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    byte message[PAYLOAD_LENGTH];
+    int i = 0;
+    while (LoRa.available()) {
+      message[i] = LoRa.read();
+      Serial.print(message[i], HEX);
+      i++;
+    }
+    sendACK(message);
+    sendUARTMessage(message);
+    }
+}
+
+// -------------------- Setup Devices -------------------- //
+
+void setup() {
+
+  // UART setup
+  Serial.begin(115200);
+  while (!Serial);
+  myTransfer.begin(Serial);
+  Serial.println("Receiver");
+
+  // LoRa setup
+  if (!LoRa.begin(LoRa_freq)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.setSpreadingFactor(LoRa_SF);
+  LoRa.setSignalBandwidth(LoRa_bw);
+  Serial.println("- LoRa ready!");
+}
+
+// -------------------- Main Program -------------------- //
+
+void loop() {
+  receiveMessage();
+}
